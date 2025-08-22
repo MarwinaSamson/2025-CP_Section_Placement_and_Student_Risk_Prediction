fdafb641605157177ed1f916cd6688af9a42edaf
HiitsMehayst <eh202201320@wmsu.edu.ph>
2025-08-22 17:50:55 +0700

    Fixed landing page
    The landing page has already fixed

--- enrollmentprocess/urls.py
+++ enrollmentprocess/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
         path("", views.homepage, name="homepage"), # Route the root URL to the homepage view
+        path("", views.student_data, name='student_data'),
     ]
     
--- enrollmentprocess/views.py
+++ enrollmentprocess/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def homepage(request):
-        return render(request, 'enrollmentprocess/Homepage.html')
+        return render(request, 'enrollmentprocess/index.html')
+
+def student_data(request):
+    return render(request, 'enrollmentprocess/studentData.html')
