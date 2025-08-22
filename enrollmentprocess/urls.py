2788de232f7ec248d87973fa0dd9f132e5d3e4ba
HiitsMehayst <eh202201320@wmsu.edu.ph>
2025-08-22 22:04:58 +0700

    All routing fixed
    For all the htm uploaded, i already updated and fix it.

--- enrollmentprocess/urls.py
+++ enrollmentprocess/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
         path("", views.homepage, name="homepage"), # Route the root URL to the homepage view
-        path("", views.student_data, name='student_data'),
+        path("student-data/", views.student_data, name='student_data'), # Route to the student_data view
+        path("family-data/", views.family_data, name='family_data'), # Route to the family_data view
+        path("student-academic/", views.student_academic, name='student_academic'), # Route to the student_academic view
+        path("student-academic-2/", views.student_academic_2, name='student_academic_2'), # Route to the student_academic_2 view
     ]
     
--- enrollmentprocess/views.py
+++ enrollmentprocess/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def homepage(request):
-        return render(request, 'enrollmentprocess/index.html')
+        return render(request, 'enrollmentprocess/index.html') # Render the index.html template
 
 def student_data(request):
-    return render(request, 'enrollmentprocess/studentData.html')
+    return render(request, 'enrollmentprocess/studentData.html') # Render the studentData.html template
+
+def family_data(request):
+        return render(request, 'enrollmentprocess/familyData.html') # Render the familyData.html template
+
+def student_academic(request):
+        return render(request, 'enrollmentprocess/studentAcademic.html') # Render the studentAcademic.html template
+
+def student_academic_2(request):
+        return render(request, 'enrollmentprocess/studentAcademic2.html') # Render the studentAcademic2.html template
