33782bb9d86d89840b7973d87dde3485ed83f673
HiitsMehayst <eh202201320@wmsu.edu.ph>
2025-08-26 03:00:32 +0700

    updated routing
    some files were updated

--- enrollmentprocess/urls.py
+++ enrollmentprocess/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
         path("", views.homepage, name="homepage"), # Route the root URL to the homepage view
         path("student-data/", views.student_data, name='student_data'), # Route to the student_data view
         path("family-data/", views.family_data, name='family_data'), # Route to the family_data view
+        path("student-non-academic/", views.student_non_academic, name='student_non_academic'), # Route to the student_non_academic view
         path("student-academic/", views.student_academic, name='student_academic'), # Route to the student_academic view
         path("student-academic-2/", views.student_academic_2, name='student_academic_2'), # Route to the student_academic_2 view
     ]
--- enrollmentprocess/views.py
+++ enrollmentprocess/views.py
@@ -9,8 +9,12 @@ def student_data(request):
 def family_data(request):
         return render(request, 'enrollmentprocess/familyData.html') # Render the familyData.html template
 
+def student_non_academic(request):
+        return render(request, 'enrollmentprocess/studentNonAcademic.html') # Render the studentNonAcademic.html template
+
 def student_academic(request):
         return render(request, 'enrollmentprocess/studentAcademic.html') # Render the studentAcademic.html template
 
 def student_academic_2(request):
         return render(request, 'enrollmentprocess/studentAcademic2.html') # Render the studentAcademic2.html template
+
